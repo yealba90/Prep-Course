e44001ae8e17220c40c389055f437c1d7588347b
yealba90 <ya.balvin1990@gmail.com>
2022-02-02 19:00:27 -0500

    second commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -208,17 +208,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero === 0 || numero === 1){
+  if (numero === 0 || numero === 1 || numero === 4){
     return false;
   }
-  for (let i = 2; i < numero; i++){
-    if ((numero % i) === 0){
+  for (let i = 2; i < numero / 2; i++){
+    if (numero % i == 0){
       return false;
     }
-    else{
-      return true;
-    }
   }
+  return true;
 }
 
 function esVerdadero(valor){
